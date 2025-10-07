c0d173a5e530d4eca1a7722d6dfd00bf294d3467
git-ertugrul-yildirim <ertugrul.yildirim@gmail.com>
2025-10-07 18:00:48 +0300

    clean

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template_string, request, redirect
 import sqlite3
 
+conn = sqlite3.connect("app.db")
+cursor = conn.cursor()
 app = Flask("test")
 
 HTML_TEMPLATE = """
@@ -70,8 +72,6 @@ HTML_TEMPLATE = """
 
 @app.route("/")
 def list():
-    conn = sqlite3.connect("app.db")
-    cursor = conn.cursor()
     cursor.execute("SELECT * FROM users")
     rows = cursor.fetchall()
     columns = [description[0] for description in cursor.description]
@@ -83,8 +83,6 @@ def list():
 def add():
     name = request.form.get("name")
     if name:
-        conn = sqlite3.connect("app.db")
-        cursor = conn.cursor()
         cursor.execute("INSERT INTO users (name) VALUES (?)", (name,))
         conn.commit()
         conn.close()
@@ -92,8 +90,6 @@ def add():
 
 @app.route("/remove")
 def remove(id):
-    conn = sqlite3.connect("app.db")
-    cursor = conn.cursor()
     cursor.execute("DELETE FROM users WHERE id = ?", (id,))
     conn.commit()
     conn.close()
@@ -101,8 +97,6 @@ def remove(id):
 
 @app.route("/show")
 def show(id):
-    conn = sqlite3.connect("app.db")
-    cursor = conn.cursor()
     cursor.execute("SELECT * FROM users WHERE id = ?", (id,))
     user = cursor.fetchone()
     cursor.execute("SELECT * FROM users")
@@ -113,8 +107,6 @@ def show(id):
 
 @app.route("/update", methods=["POST"])
 def update():
-    conn = sqlite3.connect("app.db")
-    cursor = conn.cursor()
     cursor.execute("UPDATE users SET name = ? WHERE id = ?", (request.form.get("name"), request.form.get("id")))
     conn.commit()
     conn.close()
